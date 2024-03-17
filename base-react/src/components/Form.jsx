95ccf01891b234c2efdec7f01106e16444613a13
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2024-03-16 23:28:19 -0300

    primeiro commit

---
+++ base-react/src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../css/Form.css";
+
+const Form = () => {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
---
+++ base-react/src/components/Table.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../css/Table.css";
+
+const Table = () => {
+  return (
+    <div>Table</div>
+  )
+}
+
+export default Table
